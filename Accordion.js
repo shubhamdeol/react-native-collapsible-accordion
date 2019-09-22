f19158ff9dd30b363864f039e003254d621cffbc
shubhamdeol <deol.shubham@gmail.com>
2019-09-22 21:16:41 +0530

    feat(add readme file and contentVisibility prop

--- Accordion.js
+++ Accordion.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header'; 
 import Content from './Content';
 
-export default function Accordion({ renderContent, renderHeader }) {
+export default function Accordion({ renderContent, renderHeader, onChangeVisibility }) {
     const [ contentVisibility, setContentVisibility ] = useState(false)
+
+    useEffect(() => {
+        onChangeVisibility(contentVisibility)
+    }, [contentVisibility])
+
     return (
         <View>
-            <Header toggleContent={() => setContentVisibility(!contentVisibility)} >
+            <Header toggleContent={() => {
+                setContentVisibility(!contentVisibility)
+            }}>
                 { renderHeader() }
             </Header>
             {
